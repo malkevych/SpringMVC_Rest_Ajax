586f8e9db61b4604aaecb090cb6479b07216e3f4
Malkevych Bohdan Ihorovych <Bohdan@MacBook-Pro-oivrafiv-2.local>
2016-04-22 06:15:25 +0300

    Spring Rest Ajax

---
+++ src/main/java/com/malkevych/RestController.java
@@ -0,0 +1,45 @@
+package com.malkevych;
+
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+
+/**
+ * Created by Malkevych on 21.04.2015.
+ */
+@Controller
+@RequestMapping(value = "/MyData")
+public class RestController {
+
+    @RequestMapping(value="/test")
+    public String test(){
+        return "index";
+    }
+
+    @RequestMapping(value="/{time}", method = RequestMethod.GET)
+    public @ResponseBody MyData getMyData(
+            @PathVariable long time) {
+        return new MyData(time, "REST GET Call !!!");
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    public @ResponseBody MyData putMyData(
+            @RequestBody MyData md) {
+
+        return md;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public @ResponseBody MyData postMyData() {
+
+        return new MyData(System.currentTimeMillis(),
+                "REST POST Call !!!");
+    }
+
+    @RequestMapping(value="/{time}", method = RequestMethod.DELETE)
+    public @ResponseBody MyData deleteMyData(
+            @PathVariable long time) {
+
+        return new MyData(time, "REST DELETE Call !!!");
+    }
+}
